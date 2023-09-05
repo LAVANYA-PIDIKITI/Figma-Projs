c8d6fe3e69e161dc95fcbc2a36726bc50b5bc882
LAVANYA-PIDIKITI <98797256+LAVANYA-PIDIKITI@users.noreply.github.com>
2023-09-05 19:10:36 +0530

    Button hover

---
+++ Button hover/src/components/button-test-frame.js
@@ -0,0 +1,12 @@
+import "./button-test-frame.css";
+const ButtonTestFrame = () => {
+  return (
+    <div className="button-test-frame">
+      <div className="button1">
+        <div className="button2">Button</div>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonTestFrame;
---
+++ Button hover/src/components/property1button-default.js
@@ -0,0 +1,10 @@
+import "./property1button-default.css";
+const Property1buttonDefault = () => {
+  return (
+    <div className="property-1button-default">
+      <div className="button">Button</div>
+    </div>
+  );
+};
+
+export default Property1buttonDefault;
